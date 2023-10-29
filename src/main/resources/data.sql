e7131794affd78250ae7999bbe7db8294503f81c
Ann <anna1637@mail.ru>
2023-10-29 18:23:34 +0300

    feat: add data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,16 @@
+merge into CATEGORIES (ID, NAME)
+    values ( 1, 'Офисные' );
+merge into CATEGORIES (ID, NAME)
+    values ( 1, 'Системные' );
+merge into CATEGORIES (ID, NAME)
+    values ( 1, 'Мультимедиа' );
+merge into CATEGORIES (ID, NAME)
+    values ( 1, 'Конверторы' );
+merge into CATEGORIES (ID, NAME)
+    values ( 1, 'Архиваторы' );
+merge into CATEGORIES (ID, NAME)
+    values ( 1, 'Безопасность' );
+merge into CATEGORIES (ID, NAME)
+    values ( 1, 'Интернет' );
+merge into CATEGORIES (ID, NAME)
+    values ( 1, 'ERM и CRM' );
