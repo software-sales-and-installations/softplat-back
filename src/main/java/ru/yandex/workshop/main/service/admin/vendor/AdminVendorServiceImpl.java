d84c9d0980bd502af5e13312ff0e488c330bb0ed
Панков Денис <Pankkovv@yandex.ru>
2023-10-31 17:07:39 +0300

    feat: add logging and list retrieval logic

--- src/main/java/ru/yandex/workshop/main/service/admin/vendor/AdminVendorServiceImpl.java
+++ src/main/java/ru/yandex/workshop/main/service/admin/vendor/AdminVendorServiceImpl.java
@@ -10,7 +10,7 @@ import ru.yandex.workshop.main.dto.vendor.VendorMapper;
 import ru.yandex.workshop.main.dto.vendor.VendorResponseDto;
 import ru.yandex.workshop.main.exception.VendorNotFoundException;
 import ru.yandex.workshop.main.message.ExceptionMessage;
-import ru.yandex.workshop.main.model.vendor.Country;
+import ru.yandex.workshop.main.message.LogMessage;
 import ru.yandex.workshop.main.model.vendor.Vendor;
 import ru.yandex.workshop.main.repository.vendor.VendorRepository;
 
@@ -26,6 +26,7 @@ public class AdminVendorServiceImpl implements AdminVendorService {
 
     @Override
     public VendorResponseDto createVendor(VendorDto vendorDto) {
+        log.debug(LogMessage.ADMIN_ADD_VENDOR.label);
         return VendorMapper.INSTANCE.vendorToVendorResponseDto(repository.save(VendorMapper.INSTANCE.vendorDtoToVendor(vendorDto)));
     }
 
@@ -33,35 +34,40 @@ public class AdminVendorServiceImpl implements AdminVendorService {
     public VendorResponseDto changeVendorById(Long vendorId, VendorDto vendorDto) {
         Vendor oldVendor = repository.findById(vendorId).orElseThrow(() -> new VendorNotFoundException(ExceptionMessage.NOT_FOUND_VENDOR_EXCEPTION.label));
 
-        if(vendorDto.getName() != null){
+        if (vendorDto.getName() != null) {
             oldVendor.setName(vendorDto.getName());
         }
-        if(vendorDto.getDescription() != null){
+        if (vendorDto.getDescription() != null) {
             oldVendor.setDescription(vendorDto.getDescription());
         }
-        if(vendorDto.getImageId() != null){
+        if (vendorDto.getImageId() != null) {
             oldVendor.setImageId(vendorDto.getImageId());
         }
-        if(vendorDto.getCountry() != null){
-            oldVendor.setCountry(Country.findCountry(vendorDto.getCountry()));
+        if (vendorDto.getCountry() != null) {
+            oldVendor.setCountry(vendorDto.getCountry());
         }
 
+        log.debug(LogMessage.ADMIN_PATCH_VENDOR.label);
         return VendorMapper.INSTANCE.vendorToVendorResponseDto(repository.save(oldVendor));
     }
 
+    @Transactional(readOnly = true)
     @Override
-    public List<VendorResponseDto> findVendor(String name, String country) {
-        //TODO
-        return null;
+    public List<VendorResponseDto> findVendorAll() {
+        log.debug(LogMessage.ADMIN_GET_VENDOR.label);
+        return VendorMapper.INSTANCE.vendorToListVendorResponseDto(repository.findAll());
     }
 
+    @Transactional(readOnly = true)
     @Override
     public VendorResponseDto findVendorById(Long vendorId) {
+        log.debug(LogMessage.ADMIN_GET_ID_VENDOR.label);
         return VendorMapper.INSTANCE.vendorToVendorResponseDto(repository.findById(vendorId).orElseThrow(() -> new VendorNotFoundException(ExceptionMessage.NOT_FOUND_VENDOR_EXCEPTION.label)));
     }
 
     @Override
     public void deleteVendor(Long vendorId) {
+        log.debug(LogMessage.ADMIN_DELETE_VENDOR.label);
         repository.deleteById(vendorId);
     }
 }
