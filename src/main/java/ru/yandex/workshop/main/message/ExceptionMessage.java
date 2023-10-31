8593ed0e1a3f243477a17e7d4c3bb0631c8f2368
Панков Денис <Pankkovv@yandex.ru>
2023-10-31 17:05:48 +0300

    feat: add new messages for new functionality

--- src/main/java/ru/yandex/workshop/main/message/ExceptionMessage.java
+++ src/main/java/ru/yandex/workshop/main/message/ExceptionMessage.java
@@ -1,8 +1,8 @@
 package ru.yandex.workshop.main.message;
 
 public enum ExceptionMessage {
-    NOT_FOUND_VENDOR_EXCEPTION("Объект не найден или недоступен."),
-    NOT_FOUND_COUNTRY_EXCEPTION("Объект не найден или недоступен.");
+    NOT_FOUND_VENDOR_EXCEPTION("Вендор не найден или недоступен."),
+    NOT_FOUND_COUNTRY_EXCEPTION("Страна не найдена или недоступена.");
     public final String label;
 
     ExceptionMessage(String label) {
