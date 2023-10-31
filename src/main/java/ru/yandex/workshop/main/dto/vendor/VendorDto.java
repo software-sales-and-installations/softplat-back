bc2de2679701c2796e969e04b321a1878b56c787
Панков Денис <Pankkovv@yandex.ru>
2023-10-31 20:30:29 +0300

    fix: add validation

--- src/main/java/ru/yandex/workshop/main/dto/vendor/VendorDto.java
+++ src/main/java/ru/yandex/workshop/main/dto/vendor/VendorDto.java
@@ -4,6 +4,7 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Data;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.validator.constraints.Length;
 import ru.yandex.workshop.main.model.vendor.Country;
 
 import javax.validation.constraints.NotBlank;
@@ -13,9 +14,11 @@ import javax.validation.constraints.NotNull;
 @Builder
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class VendorDto {
-    @NotBlank
+    @NotBlank(message = "Необходимо указать имя")
+    @Length(min = 2, max = 20, message = "Длина имени должна быть от 2 до 20 символов")
     String name;
-    @NotBlank
+    @NotBlank(message = "Необходимо указать описание")
+    @Length(min = 2, max = 500, message = "Длина имени должна быть от 2 до 500 символов")
     String description;
     Long imageId; //TODO
     @NotNull
