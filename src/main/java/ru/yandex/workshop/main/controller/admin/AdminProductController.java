e1e4b037dc5df8fef34f60687fe1d52a01afe108
mdemidkin <mdemidkin@yandex.ru>
2023-10-31 11:18:10 +0100

    clean: removed comments

--- src/main/java/ru/yandex/workshop/main/controller/admin/AdminProductController.java
+++ src/main/java/ru/yandex/workshop/main/controller/admin/AdminProductController.java
@@ -7,12 +7,5 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController("/admin/product")
 public class AdminProductController {
-//    Админ должен иметь возможность:
-//    просмотреть список отправленных на модерацию карточек
-//    просмотреть выбранную карточку подробно
-//    одобрять карточки (кнопка "одобрить")
-//    отклонять карточки (кнопка "отклонить")
-
-//    @GetMapping
-//    public
+    // игнорируем, не относится к текущему PR
 }
