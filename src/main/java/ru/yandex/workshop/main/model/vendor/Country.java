c491577b6c4d703635f5a0c0a172f8af2b7fb432
Панков Денис <Pankkovv@yandex.ru>
2023-10-31 17:06:32 +0300

    feat: remove unused

--- src/main/java/ru/yandex/workshop/main/model/vendor/Country.java
+++ src/main/java/ru/yandex/workshop/main/model/vendor/Country.java
@@ -3,7 +3,6 @@ package ru.yandex.workshop.main.model.vendor;
 import ru.yandex.workshop.main.exception.CountryNotFoundException;
 
 import java.util.Arrays;
-import java.util.Optional;
 
 public enum Country {
     RUSSIA("Russia"),
@@ -16,12 +15,5 @@ public enum Country {
 
     Country(String label) {
         this.label = label;
-    }
-
-    public static Country findCountry(String name){
-        return Arrays.stream(values())
-                .filter(country -> country.label.equals(name))
-                .findFirst()
-                .orElseThrow(() -> new CountryNotFoundException("Данная страна не найдена"));
     }
 }
