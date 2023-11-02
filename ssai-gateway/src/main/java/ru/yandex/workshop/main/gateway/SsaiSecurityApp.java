b33794d65027cce10725500737a6942f72a25a0e
Панков Денис <pankkovv@yandex.ru>
2023-11-02 17:39:00 +0300

    refactor: add new annotation

--- ssai-gateway/src/main/java/ru/yandex/workshop/main/gateway/SsaiSecurityApp.java
+++ ssai-gateway/src/main/java/ru/yandex/workshop/main/gateway/SsaiSecurityApp.java
@@ -1,5 +1,8 @@
 package ru.yandex.workshop.main.gateway;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SsaiSecurityApp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
--- ssai-stats/src/main/java/ru/yandex/workshop/main/stats/SsaiStatsApp.java
+++ ssai-stats/src/main/java/ru/yandex/workshop/main/stats/SsaiStatsApp.java
@@ -1,5 +1,8 @@
 package ru.yandex.workshop.main.stats;
 
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class SsaiStatsApp {
     public static void main(String[] args) {
         System.out.println("Hello world!");
