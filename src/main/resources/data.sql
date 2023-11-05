6a436b8563404459e86106214a15e33b60b5a25d
Панков Денис <pankkovv@yandex.ru>
2023-11-05 16:57:05 +0300

    feat: add data for testers

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,16 +1,29 @@
 insert into CATEGORY (ID, NAME)
-    values ( 1, 'Офисные' );
+values (1, 'Офисные');
 insert into CATEGORY (ID, NAME)
-    values ( 2, 'Системные' );
+values (2, 'Системные');
 insert into CATEGORY (ID, NAME)
-    values ( 3, 'Мультимедиа' );
+values (3, 'Мультимедиа');
 insert into CATEGORY (ID, NAME)
-    values ( 4, 'Конверторы' );
+values (4, 'Конверторы');
 insert into CATEGORY (ID, NAME)
-    values ( 5, 'Архиваторы' );
+values (5, 'Архиваторы');
 insert into CATEGORY (ID, NAME)
-    values ( 6, 'Безопасность' );
+values (6, 'Безопасность');
 insert into CATEGORY (ID, NAME)
-    values ( 7, 'Интернет' );
+values (7, 'Интернет');
 insert into CATEGORY (ID, NAME)
-    values ( 8, 'ERM и CRM' );
+values (8, 'ERM и CRM');
+
+insert into VENDOR (name, description, image_id, country)
+values ('Adobe',
+        'Приложения от компании Adobe позволяют работать с векторной и растровой графикой, а также с видео и музыкой. На данной странице собраны самые популярные и доступные программы Adobe с ключами активации последних версий, а также версии Repack.',
+        1, 'USA');
+insert into VENDOR (name, description, image_id, country)
+values ('Microsoft Corporation',
+        'Продукты компании включают операционные системы, приложения для повышения производительности различных устройств, серверные приложения, бизнес-решения, настольные компьютеры и инструменты управления серверами, средства разработки программного обеспечения и видеоигры.',
+        2, 'USA');
+insert into VENDOR (name, description, image_id, country)
+values ('Kaspersky',
+        'Международная компания, специализирующаяся на разработке систем защиты от компьютерных вирусов, спама, хакерских атак и прочих киберугроз.',
+        3, 'RUSSIA');
