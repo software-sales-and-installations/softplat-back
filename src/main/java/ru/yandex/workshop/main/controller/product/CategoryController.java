0072f11fb97d77955e12a8cbc4bf19939379986f
Ann <anna1637@mail.ru>
2023-11-06 17:37:26 +0300

    refactor: CategoryController - add security

--- src/main/java/ru/yandex/workshop/main/controller/product/CategoryController.java
+++ src/main/java/ru/yandex/workshop/main/controller/product/CategoryController.java
@@ -1,8 +1,9 @@
-package ru.yandex.workshop.main.controller;
+package ru.yandex.workshop.main.controller.product;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.workshop.main.dto.category.CategoryDto;
 import ru.yandex.workshop.main.message.LogMessage;
@@ -14,11 +15,12 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@RequestMapping("/category")
 public class CategoryController {
     private final CategoryServiceImpl service;
 
-    @GetMapping(path = "/categories")
-    public List<CategoryDto> findCategoryAll() {
+    @GetMapping
+    public List<CategoryDto> findAllCategories() {
         log.debug(LogMessage.TRY_GET_CATEGORY.label);
         return service.findCategoryAll();
     }
@@ -29,23 +31,25 @@ public class CategoryController {
         return service.findCategoryById(catId);
     }
 
+    @PreAuthorize("hasAuthority('admin:write')")
     @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping(path = "/admin/category")
+    @PostMapping
     public CategoryDto createCategory(@RequestBody @Valid CategoryDto categoryDto) {
         log.debug(LogMessage.TRY_ADMIN_ADD_CATEGORY.label);
         return service.createCategory(categoryDto);
     }
 
-    @PatchMapping(path = "/admin/category/{catId}")
+    @PreAuthorize("hasAuthority('admin:write')")
+    @PatchMapping(path = "/{catId}")
     public CategoryDto changeCategoryById(@PathVariable(name = "catId") Long catId,
                                           @RequestBody @Valid CategoryDto categoryDto) {
         log.debug(LogMessage.TRY_ADMIN_PATCH_CATEGORY.label, catId);
         return service.changeCategoryById(catId, categoryDto);
     }
 
-
+    @PreAuthorize("hasAuthority('admin:write')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping(path = "/admin/category/{catId}")
+    @DeleteMapping(path = "/{catId}")
     public void deleteCategory(@PathVariable(name = "catId") Long catId) {
         log.debug(LogMessage.TRY_ADMIN_DELETE_CATEGORY.label, catId);
         service.deleteCategory(catId);
