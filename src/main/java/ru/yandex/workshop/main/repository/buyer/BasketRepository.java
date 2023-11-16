59888d68d721e7229c97f5389590979d2fe99912
Mikhailov Pavel <mikhailov.pi@yandex.ru>
2023-11-16 18:31:55 +0300

    fix: style code - delete import

--- src/main/java/ru/yandex/workshop/main/repository/buyer/BasketRepository.java
+++ src/main/java/ru/yandex/workshop/main/repository/buyer/BasketRepository.java
@@ -1,7 +1,6 @@
 package ru.yandex.workshop.main.repository.buyer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 import ru.yandex.workshop.main.model.buyer.Basket;
 
