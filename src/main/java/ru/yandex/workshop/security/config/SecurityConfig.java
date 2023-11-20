9f8c92c8225b425f9b6e28d780325d7137c530c2
Панков Денис <Pankkovv@yandex.ru>
2023-11-20 18:23:32 +0300

    feat: add settings cors

--- src/main/java/ru/yandex/workshop/security/config/SecurityConfig.java
+++ src/main/java/ru/yandex/workshop/security/config/SecurityConfig.java
@@ -11,12 +11,10 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Configuration
