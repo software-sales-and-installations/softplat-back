e7d42a41338ddf8150956eaa7e4fbd88ba250e79
mdemidkin <mdemidkin@yandex.ru>
2023-11-20 18:46:28 +0100

    fix: added Getter to ErrorResponse

--- src/main/java/ru/yandex/workshop/main/dto/error/ErrorResponse.java
+++ src/main/java/ru/yandex/workshop/main/dto/error/ErrorResponse.java
@@ -1,10 +1,12 @@
 package ru.yandex.workshop.main.dto.error;
 
 import lombok.Builder;
+import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Builder
+@Getter
 public class ErrorResponse {
     private String message;
     private String reason;
