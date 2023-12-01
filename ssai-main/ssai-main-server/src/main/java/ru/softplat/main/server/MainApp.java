31cd71525c5aeb4d7fd0535746f9ec9070aed54f
Ann <anna1637@mail.ru>
2023-12-01 23:18:53 +0300

    fix: component scan

--- ssai-main/ssai-main-server/src/main/java/ru/softplat/main/server/MainApp.java
+++ ssai-main/ssai-main-server/src/main/java/ru/softplat/main/server/MainApp.java
@@ -2,7 +2,9 @@ package ru.softplat.main.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"ru.softplat.stats.client.client"})
 @SpringBootApplication
 public class MainApp {
     public static void main(String[] args) {
--- ssai-security/ssai-security-server/src/main/java/ru/softplat/security/server/SecurityApp.java
+++ ssai-security/ssai-security-server/src/main/java/ru/softplat/security/server/SecurityApp.java
@@ -2,7 +2,9 @@ package ru.softplat.security.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages = {"ru.softplat.stats.client.client", "ru.softplat.main.client"})
 @SpringBootApplication
 public class SecurityApp {
     public static void main(String[] args) {
