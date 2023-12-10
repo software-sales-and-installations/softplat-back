2a5b2a884e166042b3175a3371086a8d8ae1a6c2
Ann <anna1637@mail.ru>
2023-12-11 00:12:04 +0300

    fix: validation

--- ssai-main/ssai-main-dto/src/main/java/ru/softplat/main/dto/product/ProductCreateUpdateDto.java
+++ ssai-main/ssai-main-dto/src/main/java/ru/softplat/main/dto/product/ProductCreateUpdateDto.java
@@ -19,12 +19,12 @@ import javax.validation.constraints.PositiveOrZero;
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class ProductCreateUpdateDto {
     @NotBlank(groups = {New.class}, message = "Необходимо указать имя продукта")
-    @Pattern(groups = {New.class, Update.class}, regexp = "^[a-zA-Zа-яА-Я-.,\\s]{2,255}$", message = "Неверные символы в названии товара. " +
+    @Pattern(groups = {New.class, Update.class}, regexp = "^[a-zA-Zа-яёЁА-Я-.,\"'«»\\s]{2,255}$", message = "Неверные символы в названии товара. " +
             "Длина названия продукта должна быть от 2 до 255 символов. Цифры в названии не допускаются.")
     String name;
 
     @NotBlank(groups = {New.class}, message = "Необходимо указать описание продукта")
-    @Pattern(groups = {New.class, Update.class}, regexp = "[0-9a-zA-Zа-яА-Я-@#$.,?%^&+=!\\s]{2,500}$",
+    @Pattern(groups = {New.class, Update.class}, regexp = "[0-9a-zA-Zа-яёЁА-Я-@#$.,?%^&+=!\"'«»\\s]{2,500}$",
             message = "Длина описания должна быть от 2 до 500 символов.")
     String description;
 
