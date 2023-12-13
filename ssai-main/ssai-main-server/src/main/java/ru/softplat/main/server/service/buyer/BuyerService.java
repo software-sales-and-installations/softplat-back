659cc1f2d99f89bf75bacf3d6a50474b41e7842b
Панков Денис <Pankkovv@yandex.ru>
2023-12-13 20:21:06 +0300

    fix: remove whitespace

--- ssai-main/ssai-main-server/src/main/java/ru/softplat/main/server/service/buyer/BuyerService.java
+++ ssai-main/ssai-main-server/src/main/java/ru/softplat/main/server/service/buyer/BuyerService.java
@@ -60,7 +60,7 @@ public class BuyerService {
                 ));
     }
 
-    public void deleteBuyer(long userId){
+    public void deleteBuyer(long userId) {
         getBuyer(userId);
 
         buyerRepository.deleteById(userId);
